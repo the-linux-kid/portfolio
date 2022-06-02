a9e7b6568eea4fe18438ab55ee3b0b4e8d10fab1
Leon Eifler <leon.eifler@tutanota.com>
2022-06-02 11:33:09 +0200

    Ship version 0.0.5

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const items = document.querySelectorAll('#timeline li');
+// const items = document.querySelectorAll('#timeline li');
 
-const isInViewport = el => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight + 100 || document.documentElement.clientHeight + 100 ) &&
-    rect.right <= (window.innerWidth + 100 || document.documentElement.clientWidth + 100)
-  );
-};
+// const isInViewport = el => {
+//   const rect = el.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <=
+//       (window.innerHeight + 100 || document.documentElement.clientHeight + 100 ) &&
+//     rect.right <= (window.innerWidth + 100 || document.documentElement.clientWidth + 100)
+//   );
+// };
 
-const run = () =>
-  items.forEach(item => {
-    if (isInViewport(item)) {
-      item.classList.add('show');
-    }
-  });
+// const run = () =>
+//   items.forEach(item => {
+//     if (isInViewport(item)) {
+//       item.classList.add('show');
+//     }
+//   });
 
-// Events
-window.addEventListener('load', run);
-window.addEventListener('resize', run);
-window.addEventListener('scroll', run);
+// // Events
+// window.addEventListener('load', run);
+// window.addEventListener('resize', run);
+// window.addEventListener('scroll', run);
